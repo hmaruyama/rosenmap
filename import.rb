7539d6f95b931cb0dc73b4cc892d5aae46e8af29
hmaruyama <maruyama@val.co.jp>
2017-12-07 19:30:53 +0900

    駅ごとの駐輪場の数と、駅ごとの駐輪場の収容台数を可視化しました。

--- import.rb
+++ import.rb
@@ -7,10 +7,11 @@ parkings = []
 offset = 1
 station_code_list = []
 parking_count_by_station = {}
+capacity_by_station = {}
 
 15.times do
   params = {
-    fields: 'neighborStations.code,name',
+    fields: 'neighborStations.code,name,capacity',
     offset: offset
   }
   response = RestClient.get("#{DOMAIN}#{PATH}", params: params)
@@ -43,4 +44,31 @@ end
 
 File.open('parking_count_by_station.js', 'w') do |f|
   f.puts("var parkingCountByStation = #{parking_count_by_station.to_json}")
+end
+
+station_code_list.uniq.each do |code|
+  parkings.each do |parking|
+    parking['neighborStations'].each do |station|
+      if station['code'] == code
+        if capacity_by_station[code.to_s]
+          capacity_by_station[code.to_s] += parking['capacity']
+        else
+          capacity_by_station[code.to_s] = parking['capacity']
+        end
+      end
+    end
+  end
+end
+
+
+# parkings.each do |parking|
+#   if capacity_by_station[parking['code'].to_s]
+#     capacity_by_station[parking['code'].to_s] += parking['capacity']
+#   else
+#     capacity_by_station[parking['code'].to_s] = parking['capacity']
+#   end
+# end
+
+File.open('capacity_by_station.js', 'w') do |f|
+  f.puts("var capacityByStation = #{capacity_by_station.to_json}")
 end
